c4069b2302daa47f69f57acf23894f889f5f0b27
johnnymaconny <johnnymaconny@gmail.com>
2025-03-01 23:06:45 -0800

    Update app and hooks implementation

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,7 @@
 // Re-export all hooks for convenience
-export * from './useFirestore';
-export * from './useDebounce';
-export * from './useLocalStorage';
-export * from './useMediaQuery';
-export * from './useOnClickOutside';
-export * from './useFirestoreDb';
+export * from './useFirestore.ts';
+export * from './useDebounce.ts';
+export * from './useLocalStorage.ts';
+export * from './useMediaQuery.ts';
+export * from './useOnClickOutside.ts';
+export * from './useFirestoreDb.ts';
